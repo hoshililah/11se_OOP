8e66f34bbe23879eff155d8d941f118e56286abc
hoshi <hoshi.lilah@gmail.com>
2026-05-14 02:37:07 +0000

    4c Done

--- OOP/4b._OOP_private_protected_attributes_activities.py
+++ OOP/4b._OOP_private_protected_attributes_activities.py
@@ -3,22 +3,34 @@
 # - Create a private attribute
 
 class Pet:
-    def __init__(self, name, category, age = 0):
+    def __init__(self, name, category, breed, age = 0):
         self.name = name
         self.category = category
+        self.__breed = breed
         self.age = age
         self.ccard = 'unknown'
         self.vaccinated = False
+
+    def set_name(self,new_name):
+        if type(new_name) == str:
+            self._name = new_name
+        else:
+            print('Please use a string as a name attribute')
+        
 
     def __str__(self):
         payment_status = 'unregistered'
         if len(self.ccard) == 19:
             payment_status = 'registered'
     
-        my_status = 'Name: ' + self.name + '\nCategory: ' + self.category +  '\nAge: ' + str(self.age) + '\nPayment Status: ' + payment_status + '\nVaccinated: ' + str(self.vaccinated)
+        my_status =  '\nName: ' + self.name + '\nCategory: ' + self.category +  '\nAge: ' + str(self.age) + '\nPayment Status: ' + payment_status + '\nVaccinated: ' + str(self.vaccinated) + '\nBreed: ' + self.__breed + '\n'
         return my_status
 
-p1 = Pet(name = 'Bonnie', category = 'Cat', age = 10)
+p1 = Pet(name = 'Bonnie', category = 'Cat', breed = 'Tabby', age = 10)
+
+p1.__breed = 'Red'
+p1.set_name('Bonnifer')
+print(p1)
 
 #ACTIVITIES:
 #1. Make category a private attribute than test to make sure it can't be changed once created
--- OOP/4c._get_set_attributes_activities.py
+++ OOP/4c._get_set_attributes_activities.py
@@ -8,7 +8,41 @@ class Pet:
         self.vaccinated = False
         self.weight = 0
 
-   
+    def set_name(self,new_name):
+        if type(new_name) == str:
+            self._name = new_name
+        else:
+            print('Please use a string as a name attribute')
+        
+    def get_name(self):
+        return self._name
+    
+    def get_weight(self):
+        return self.weight
+    
+    def set_weight(self, new_weight):
+        if type(new_weight) == int or type(new_weight) == float:
+            if new_weight > 0:
+                self.weight = new_weight
+            else:
+                print('Please enter a positive number for weight')
+        else:
+            print('Please enter a number for weight')
+
+    def __str__(self):
+        payment_status = 'unregistered'
+        if len(self.__ccard) == 19:
+            payment_status = 'registered'
+    
+        my_status =  '\nName: ' + self._name + '\nCategory: ' + self.__category + '\nAge: ' + str(self.age) + '\nPayment Status: ' + payment_status + '\nVaccinated: ' + str(self.vaccinated) + '\nBreed: ' + self.__breed + '\n'
+        return my_status
+
+p1 = Pet(name = 'Bonnie', category = 'Cat', breed = 'Tabby', age = 10)
+p1.set_weight(12.37)
+
+print()
+print('Bonnies Weight: ' ,p1.weight)
+print(p1)
 
 #ACTIVITIES:
 #1. Add attribute weight and write a getter method for weight
